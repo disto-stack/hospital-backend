5553f829d37947aee487efef3c038005ce6b178e
disto-stack <juan.ocampo.2501@gmail.com>
2021-06-14 16:30:36 -0500

    Routes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Routes
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Hello world'
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server running on port:', 3000);
 })
